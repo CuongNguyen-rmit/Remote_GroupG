bdfbabe0e1c7c97f7ffff3afc130babba7a02cba
Minh Phan <minh40490@gmail.com>
2023-12-17 11:19:28 +0700

    delete for new update

--- lib/joystick/joystick.h
+++
@@ -1,3 +0,0 @@
-#define LR_PIN 35 // Pin 33 attached to Left Right pin
-#define UD_PIN 32
-#define SW_PIN 33
--- lib/potentiometer/potentiometer.h
+++
@@ -1,9 +0,0 @@
-// ================================================================
-// Variable declaration
-// ================================================================
-#define MAX_SIGNAL 2000 // Parameter required for the ESC definition
-#define MIN_SIGNAL 1000 // Parameter required for the ESC definition
-#define POT_PIN 34      // Pin 35 attached to the potentiometer
-
-// ================================================================
-#define POT_PIN 34
