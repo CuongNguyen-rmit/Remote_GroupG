ca68c8e51c4f83a66a8739084274d58dba8e8d74
Minh Phan <minh40490@gmail.com>
2023-12-19 14:13:58 +0700

    delete for update

--- lib/Calibration/Calibration.h
+++
@@ -1,6 +0,0 @@
-#include <Arduino.h>
-
-void calibrationModeWithoutBattery(); // step 1 calibrate without battery first
-void calibrationModeWithBattery(); // step2 calibrate when connect to battery after enter calibration mode
-void calibrationModeWithValue(); // to tell user about the esc value
-void waitForKeyStroke();
